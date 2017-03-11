81f5a54aede612e3e0355ece6817dfd6f5531e9e
mgcaret <mgcaret@users.noreply.github.com>
2017-03-11 11:51:28 -0800

    5X: cite prior art

--- rom5x/options/accel_reverse/B1_FDD5_accel_reverse.s
+++ rom5x/options/accel_reverse/B1_FDD5_accel_reverse.s
@@ -2,6 +2,9 @@
 ; <ESC> at reset then selects 4 MHz
 ; This is useful if you spend more time at 1 MHz, for games
 
+; This patch was inspired by Quinn Dunki's functionally equivalent
+; firmware mod found here: http://quinndunki.com/blondihacks/?p=2546
+
 .text
 * = $fdd5
 	.byte $f0
