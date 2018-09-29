ee1faf6cc7876ffc36ed0cb26bc2fe60834d2b18
mgcaret <mgcaret@users.noreply.github.com>
2018-09-29 15:51:50 -0700

    modify dist.sh

--- dist.sh
+++ dist.sh
@@ -3,7 +3,14 @@
 rm -f "romXx_dist-*.zip"
 ROM4X="rom4x/iic_rom4x.bin"
 ROM5X="rom5x/iic+_rom5x.bin"
-FNAME="romXx_dist-`date --rfc-3339=date`.zip"
+case `uname -s` in
+Linux)
+	FNAME="romXx_dist-`date --rfc-3339=date`.zip"
+	;;
+*)
+	FNAME="romXx_dist-`date '+%Y-%M-%d'`.zip"
+	;;
+esac
 [ -f "${ROM4X}" ] && zip "${FNAME}" "${ROM4X}"
 [ -f "${ROM5X}" ] && zip "${FNAME}" "${ROM5X}"
 
